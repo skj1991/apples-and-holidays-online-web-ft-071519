3565b12f380c4e23ececf9d4890b4f4f4929201a
Stacy Johns <stacykjohns@icloud.com>
2019-07-25 01:18:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holidays|
+  puts "#{season.capitalize}:"
+  holidays.each do |holiday, supplies|
+    puts "  #{holiday.capitalize}: #{supplies}".join("")
 end
 
 def all_holidays_with_bbq(holiday_hash)
