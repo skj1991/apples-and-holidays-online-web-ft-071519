1a57818d322c4e273b2b6da561dacfc6a17e78ad
Stacy Johns <stacykjohns@icloud.com>
2019-07-25 00:34:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,28 +2,42 @@ require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
+  #  holiday_hash = {
+  #    :winter => {
+  #      :christmas => ["Lights", "Wreath"],
+  #      :new_years => ["Party Hats"]
+  #    },
+  #    :summer => {
+  #      :fourth_of_july => ["Fireworks", "BBQ"]
+  #    },
+  #    :fall => {
+  #      :thanksgiving => ["Turkey"]
+  #    },
   #   :spring => {
   #     :memorial_day => ["BBQ"]
   #   }
   # }
   # return the second element in the 4th of July array
+#holiday_hash.each do |season, holidays|
+#  holidays.each do |day, supplies|
+
+#    end
+#  end
+
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash.each do |season, holiday_hash|
+    if season == :winter
+      holiday_hash.each do |day, supplies|
+        supplies.push(supply)
+      end
+    end
+  end
 
 end
 
@@ -31,6 +45,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
@@ -42,6 +57,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash.values
 
 end
 
@@ -61,10 +77,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
